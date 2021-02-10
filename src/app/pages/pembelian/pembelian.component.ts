6d273d2206f0034c571c06eb1169b44be001f179
RIUS SABARNO <U069738@bca>
2021-02-10 15:03:38 +0700

    update

--- src/app/pages/pembelian/pembelian.component.ts
+++ src/app/pages/pembelian/pembelian.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PembelianService } from '../../services/pembelian/pembelian.service';
 import { SupplierService } from '../../services/supplier/supplier.service';
+import { BarangService } from '../../services/barang/barang.service';
 
 
 @Component({
@@ -13,8 +14,11 @@ export class PembelianComponent implements OnInit {
   supplier: any;
   headerPembelian: any;
   detailPembelian: any;
+  dropdownMetode: any;
+  barang:any;
   headerPembelians = {
     kodePembelian : '',
+    kodePembelianGenerated: '',
     tanggalPembelian: '',
     kodeSupplier: '',
     metodePembayaran: '',
@@ -35,10 +39,13 @@ export class PembelianComponent implements OnInit {
   updatedId: String = '';
 
 
-  constructor(private pembelianService: PembelianService,private supplierService: SupplierService) { }
+  constructor(private pembelianService: PembelianService,private supplierService: SupplierService,private barangService: BarangService) { }
 
   ngOnInit(): void {
     this.getAllHeader();
+    this.dropdownMetode = [
+      "Transfer Bank","ShoppePay","ShoppePayLater","OVO","DANA","GOPAY"
+    ]
   }
 
   public open() {
@@ -55,6 +62,7 @@ export class PembelianComponent implements OnInit {
         data => {
           this.headerPembelian = data;
           this.getAllSupplier();
+          this.getAllBarang();
         },
         error => {
           console.log(error);
@@ -70,6 +78,39 @@ export class PembelianComponent implements OnInit {
         error => {
           console.log(error);
         });
+  }
+
+  getAllBarang(): void {
+    this.barangService.getAll()
+      .subscribe(
+        data => {
+          this.barang = data;
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  resetForm(){
+    this.title = 'Tambah';
+    this.headerPembelians = {
+      kodePembelian : '',
+      kodePembelianGenerated: '',
+      tanggalPembelian: '',
+      kodeSupplier: '',
+      metodePembayaran: '',
+      totalPembelian: '',
+      ongkir: '',
+      potongan: '',
+    }
+    this.detailPembelians = {
+      kodePembelian : '',
+      kodeBarang: '',
+      namaBarang: '',
+      hargaBeli: '',
+      quantity: '',
+      totalHarga: '',
+    }
   }
 
   
